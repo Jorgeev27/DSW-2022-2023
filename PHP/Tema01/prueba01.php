7d85566ccdd58458afa90fe666a0cb5d1a865503
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-19 17:43:16 +0100

    Add files via upload

---
+++ PHP/Tema01/prueba01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primer programa PHP</title>
+</head>
+<body>
+    <h1>Mi primer programa PHP</h1>
+    <?php
+        $a = 1;
+        $b = 2;
+        $suma = $a + $b;
+        echo "La suma de $a y $b es: $suma";
+    ?>
+</body>
+</html>
