c3f749eaaee7f8e0f5592881d5c2d054e3be7e7e
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-21 16:08:58 +0100

    Update sumaMedia.php

--- PHP/Tema02/sumaMedia.php
+++ PHP/Tema02/sumaMedia.php
@@ -1,3 +1,4 @@
+<?php declare(strict_types=1); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,14 +9,14 @@
 </head>
 <body>
     <?php
-        $numero1=$_POST['numero1'];
-        $numero2=$_POST['numero2'];
+        $numero1=$_REQUEST['numero1'];
+        $numero2=$_REQUEST['numero2'];
         $suma=0;
         for($i=$numero1;$i<=$numero2;$i++){
             $suma+=$i;
         }
         echo "La suma de los dos numeros es: $suma.\n<br/>";
-        echo "La media de los dos numeros es: ",$suma/($numero2-$numero1-1);
+        echo "La media de los dos numeros es: ",$suma/($numero2-$numero1+1);
     ?>
 </body>
 </html>
