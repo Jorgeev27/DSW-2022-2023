2773afe7438c0bdd413a09887d7ffc763186510e
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-21 14:48:43 +0100

    Add files via upload

---
+++ PHP/Tema02/sumaMedia.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <?php
+        $numero1=$_POST['numero1'];
+        $numero2=$_POST['numero2'];
+        $suma=0;
+        for($i=$numero1;$i<=$numero2;$i++){
+            $suma+=$i;
+        }
+        echo "La suma de los dos numeros es: $suma.\n<br/>";
+        echo "La media de los dos numeros es: ",$suma/($numero2-$numero1-1);
+    ?>
+</body>
+</html>
