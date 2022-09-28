ce45b7955eac785e72450924c2c5a4b1a3076dca
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-28 23:40:53 +0100

    Create ejercicio02.php

---
+++ PHP/Tema03/Ejercicio02/ejercicio02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 02_Jorge Escobar</title>
+</head>
+<body>
+    <?php
+        function sorteoPrimitiva(){
+            $numerosPrimitiva = [];
+            for($i = 0; $i <= 5; $i++){
+                in_array($nuevo, $numerosPrimitiva) ? array_push($numerosPrimitiva, $nuevo) : random_int();
+            }
+            do{
+                $nuevo = random_int(1,49);
+
+            }while(count($numerosPrimitiva) < 6)
+        }
+    ?>
+</body>
+</html>
