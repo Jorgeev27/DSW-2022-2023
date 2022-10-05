d35964b80f1f92a7ed696ecae8d0047205cc4b5c
Jorge Escobar <jorgeescobarv27@gmail.com>
2022-10-05 16:21:33 +0100

    Main

--- PHP/Tema03/Ejercicio06/ejercicio06 V2.php
+++ PHP/Tema03/Ejercicio06/ejercicio06 V2.php
@@ -15,7 +15,20 @@
     6. Implementar un contador de visitas
     
     */
-
+    $fContador="/var/www/phpdata/contador.txt";
+    if(file_exists($fContador)){ //Si existe el fichero
+        $descripcionContador = fopen($fContador, "r+"); //Abre el fichero
+        $contador = intval(fgets($descripcionContador)); //Lee el fichero y el contador lo convierte a entero
+        $contador++; //Aumenta el contador
+        fseek($descripcionContador, 0); // Posicion del fichero a 0
+        fputs($descripcionContador, strval($contador)); //Escribe el valor incrementado
+    }else{
+        $descripcionContador = fopen($fContador, "w");
+        fputs($descripcionContador, "1");
+        $contador = 1;
+    }
+    fclose($descripcionContador);
+    echo "Eres el visitante: $contador";
 
     ?>
 </body>
