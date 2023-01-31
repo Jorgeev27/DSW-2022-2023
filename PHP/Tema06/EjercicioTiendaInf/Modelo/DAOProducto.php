ef5ae6ed06d2fc0db0b7d9b8f0d5c90ebb2442ba
Jorge Escobar <jorgeescobarv27@gmail.com>
2023-01-31 16:39:30 +0000

    Main

--- PHP/Tema06/EjercicioTiendaInf/Modelo/DAOProducto.php
+++ PHP/Tema06/EjercicioTiendaInf/Modelo/DAOProducto.php
@@ -1,4 +1,16 @@
 <?php
     declare(strict_types = 1);
     require_once("../../../Utilidades/BasePDO.php");
+    class DAOProducto{
+        public static function getPaginaProducto(int $numPag, int $tamPag = 10): array{
+            $listaProductos = [];
+            $indice = $tamPag * ($numPag - 1);
+            $sql = "SELECT * from producto LEFT JOIN ordenador ON producto.cod=ordenador.cod";
+            $resultado = BaseDAO::consulta($sql);
+            while(($producto = $resultado->fetchObject()) != null){
+                $listaProductos[$producto->id] = Producto::getProdStd($producto);
+            }
+            return $listaProductos;
+        }
+    }
 ?>
